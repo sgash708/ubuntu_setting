f6be31c48bc7c2697729bd4a19d41d79799bae66
sgash708 <sugash7081023@gmail.com>
2021-05-08 23:44:40 +0900

    chromedriverインストール方法変更

--- selenium_install.sh
+++ selenium_install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-# 適宜バージョンを変更すること
-wget https://launchpad.net/~canonical-chromium-builds/+archive/ubuntu/stage/+build/21468513/+files/chromium-chromedriver_90.0.4430.93-0ubuntu0.18.04.1_armhf.deb
-sudo dpkg -i chromium-chromedriver_90.0.4430.93-0ubuntu0.18.04.1_a    rmhf.deb
+sudo apt install chromium-browser
+sudo apt install chromium-chromedriver
+# フォントインストール
+sudo apt install fonts-ipafont-gothic fonts-ipafont-mincho
