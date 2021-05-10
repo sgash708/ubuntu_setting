4e8b31b5693f6aa7b75a7dad58af5165569c9261
sgash708 <sugash7081023@gmail.com>
2021-05-10 20:19:18 +0900

    vimインストール

--- ubuntu_go.sh
+++ ubuntu_go.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sudo add-apt-repository ppa:longsleep/golang-backports
 sudo apt update
+sudo apt install -y vim
+sudo add-apt-repository ppa:longsleep/golang-backports
 # old image
 sudo apt install golang
